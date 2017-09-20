f728d803da191f47f0dfa84270e39ce21c645fd3
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2017-09-20 14:09:13 +0200

    light.meshmesh: Rgb support and debug

--- light/meshmesh.py
+++ light/meshmesh.py
@@ -3,7 +3,7 @@ import logging
 
 from homeassistant.helpers import config_validation as cv
 from homeassistant.components.light import (
-    Light, ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, SUPPORT_RGB_COLOR)
+    Light, ATTR_BRIGHTNESS, ATTR_RGB_COLOR, SUPPORT_BRIGHTNESS, SUPPORT_RGB_COLOR)
 
 from .. import meshmesh
 
@@ -13,7 +13,12 @@ CONF_ON_STATE = 'on_state'
 CONF_ON_BRIGHTNESS = 'on_brightness'
 DEFAULT_ON_STATE = True
 DEFAULT_ON_BRIGHTNESS = 127
+
 DEFAULT_CHANNEL = 255
+BLUE_CHANNEL = 0
+GREEN_CHANNEL = 1
+RED_CHANNEL = 2
+WHITE_CHANNEL = 3
 
 PLATFORM_SCHEMA = meshmesh.PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_ON_STATE, default=DEFAULT_ON_STATE): cv.boolean,
@@ -41,11 +46,30 @@ class MeshMeshLight(Light):
         self._optimistic = True
         self._state = config.on_state
         self._brightness = config.on_state_brightness
+        self._xy_color = (.5, .5)
         self._config = config
 
+    def _set_rgb_color(self, red, green, blue):
+        red = int(red/256.0*1024.0)
+        green = int(green/256.0*1024.0)
+        blue = int(blue/256.0*1024.0)
+        white = 64
+
+        try:
+            meshmesh.DEVICE.set_analog_out(self._config.address, RED_CHANNEL, red)
+            meshmesh.DEVICE.set_analog_out(self._config.address, GREEN_CHANNEL, green)
+            meshmesh.DEVICE.set_analog_out(self._config.address, BLUE_CHANNEL, blue)
+            meshmesh.DEVICE.set_analog_out(self._config.address, WHITE_CHANNEL, white)
+        except meshmesh.MESHMESH_TX_FAILURE:
+            _LOGGER.warning("MeshMeshLight.turn_on Transmission failure with device at addres: %08X", self._config.address)
+        except meshmesh.MESHMESH_EXCEPTION:
+            _LOGGER.warning("MeshMeshLight.turn_on Transmission failure with device at addres: %08X", self._config.address)
+
     def turn_on(self, **kwargs) -> None:
-        bright = kwargs[ATTR_BRIGHTNESS] if ATTR_BRIGHTNESS in kwargs else '128'
-        _LOGGER.debug("MeshMeshLight.turn_on set light %08X at brightness at %s", self._config.address, bright)
+        print(kwargs)
+        color = kwargs[ATTR_RGB_COLOR] if ATTR_RGB_COLOR in kwargs else None
+        bright = kwargs[ATTR_BRIGHTNESS] if ATTR_BRIGHTNESS in kwargs else 128
+        _LOGGER.debug("MeshMeshLight.turn_on set light %08X at brightness at %s color at %s", self._config.address, bright, color)
         pwm = int(bright/256.0*1024.0)
         try:
             meshmesh.DEVICE.set_analog_out(self._config.address, DEFAULT_CHANNEL, pwm)
@@ -75,6 +99,11 @@ class MeshMeshLight(Light):
     @property
     def brightness(self):
         return self._brightness
+
+    @property
+    def xy_color(self):
+        """Return the XY color value [float, float]."""
+        return self._xy_color
 
     @property
     def should_poll(self):
