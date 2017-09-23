59b1f0e421e739d35f7c42d254daccb9b17544f9
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2017-09-23 09:16:40 +0200

    switch.meshmesh: Relative import from custom_components

--- switch/meshmesh.py
+++ switch/meshmesh.py
@@ -1,20 +1,20 @@
 import voluptuous as vol
 
 from homeassistant.components.switch import (SwitchDevice)
-from homeassistant.components.meshmesh import (MeshMeshDigitalOut, MeshMeshDigitalOutConfig, PLATFORM_SCHEMA)
+from .. import meshmesh
 
 CONF_ON_STATE = 'on_state'
 DEPENDENCIES = ['meshmesh']
 STATES = ['high', 'low']
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+PLATFORM_SCHEMA = meshmesh.PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_ON_STATE): vol.In(STATES),
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    add_devices([MeshMeshSwitch(hass, MeshMeshDigitalOutConfig(config))])
+    add_devices([MeshMeshSwitch(hass, meshmesh.MeshMeshDigitalOutConfig(config))])
 
 
-class MeshMeshSwitch(MeshMeshDigitalOut, SwitchDevice):
+class MeshMeshSwitch(meshmesh.MeshMeshDigitalOut, SwitchDevice):
     pass
